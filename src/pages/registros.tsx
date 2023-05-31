f1a347c9f8671003f4cc217aa8317bdf03c70b34
marchettomarcelo <marcelovfm@al.insper.edu.br>
2023-05-31 12:39:48 -0300

    okkkk

--- src/pages/registros.tsx
+++ src/pages/registros.tsx
@@ -5,10 +5,10 @@ import registros from "../utils/mock.json";
 function RegistrosPage() {
   return (
     <BaseLayout>
-      {registros.map((registro) => {
+      {registros.map((registro, idx) => {
         const date = new Date(registro.data).toLocaleDateString("pt-BR");
         return (
-          <div className="border-opacity-94 flex  w-full flex-col items-start gap-4 rounded-lg border bg-white bg-opacity-75 p-4 text-2xl text-white shadow-md backdrop-blur-sm backdrop-filter ">
+          <div key={idx} className="border-opacity-94 flex  w-full flex-col items-start gap-4 rounded-lg border bg-white bg-opacity-75 p-4 text-2xl text-white shadow-md backdrop-blur-sm backdrop-filter ">
             <div className="flex w-full items-center justify-between text-2xl text-black">
               <h2 className="font-bold">{registro.local}</h2>
 
